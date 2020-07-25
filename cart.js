e2b159c66f98e400016ad1a4f58af20cdb774f23
Shakib448 <shakiba448@gmail.com>
2020-07-25 16:25:19 +0600

    first commit

--- cart.js
+++ cart.js
@@ -1,3 +1,27 @@
+// Iphone Increase 
+var iphoneIncrease = document.getElementById('iphone-increase');
+
+iphoneIncrease.addEventListener('click', function(){
+    const iphoneInput = document.getElementById('iphone-count');
+    const iphoneCount = parseInt(iphoneInput.value);
+    const iphoneNewCount = iphoneCount + 1;
+    iphoneInput.value = iphoneNewCount;
+    const totalIphone = iphoneNewCount * 1219;
+    document.getElementById('iphone-total').innerText = totalIphone;
+});
+
+// Iphone Decrease 
+var iphoneIncrease = document.getElementById('iphone-decrease');
+
+iphoneIncrease.addEventListener('click', function(){
+    const iphoneInput = document.getElementById('iphone-count');
+    const iphoneCount = parseInt(iphoneInput.value);
+    const iphoneNewCount = iphoneCount - 1;
+    iphoneInput.value = iphoneNewCount;
+    const totalIphone = iphoneNewCount * 1219;
+    document.getElementById('iphone-total').innerText = totalIphone;
+});
+
 // Case Increase 
 var caseIncrease = document.getElementById('case-increase');
 
