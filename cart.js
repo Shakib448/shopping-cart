80befbdcbcf0ab18743d5506a53e0aa7db5e12d4
Shakib448 <shakiba448@gmail.com>
2020-07-25 16:12:42 +0600

    first commit

--- cart.js
+++ cart.js
@@ -1,109 +1,24 @@
-//Item increment event handler
-const incrementBtn1 = document.getElementById('incrementBtn1');
-incrementBtn1.addEventListener('click', function () {
-    addProduct("itemValue1", 'currentPrice1', 1219);
+// Case Increase 
+var caseIncrease = document.getElementById('case-increase');
+
+caseIncrease.addEventListener('click', function(){
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount + 1;
+    caseInput.value = caseNewCount;
+    const caseTotal = caseNewCount * 59;
+    document.getElementById('case-total').innerText = caseTotal;
 });
 
-const incrementBtn2 = document.getElementById('incrementBtn2');
-incrementBtn2.addEventListener('click', function () {
-    addProduct('itemValue2', 'currentPrice2', 59);
+// Case Decrease 
 
-});
+var caseDecrease = document.getElementById('case-decrease');
 
-//Item Decrement event handler
-
-const decrementBtn1 = document.getElementById('decrementBtn1');
-decrementBtn1.addEventListener('click', function () {
-    removeProduct('itemValue1', 'currentPrice1', 1219);
-});
-
-const decrementBtn2 = document.getElementById('decrementBtn2');
-decrementBtn2.addEventListener('click', function () {
-    removeProduct('itemValue2', 'currentPrice2', 59);
-});
-
-
-//add product function
-function addProduct(id, priceId, price) {
-    const incrementValue = document.getElementById(id).value;
-    const tmpValue = parseInt(incrementValue);
-    const items = tmpValue + 1;
-    document.getElementById(id).value = items;
-
-    //pricing
-    const netPrice = price;
-    const currentPrice = netPrice * items;
-    document.getElementById(priceId).innerText = currentPrice;
-
-    //subtotal 
-    let subTotal = document.getElementById('subTotal').innerText;
-    subTotal = parseInt(subTotal);
-    subTotal = subTotal + price;
-    document.getElementById('subTotal').innerText = subTotal;
-
-    //Tax and Total amount
-    let tax = (subTotal / 100) * 5;
-    tax = tax.toFixed(2)
-    document.getElementById('tax').innerText = tax;
-    const totalAmount = subTotal - tax;
-    document.getElementById('totalAmount').innerText = totalAmount;
-
-};
-
-//Remove Product Function 
-function removeProduct(id, priceId, price) {
-    const decrementValue = document.getElementById(id).value;
-    const tmpValue = parseInt(decrementValue);
-    const items = tmpValue - 1;
-    document.getElementById(id).value = items;
-
-
-    //Pricing
-    let currentPrice = document.getElementById(priceId).innerText;
-    currentPrice = parseInt(currentPrice);
-    const netPrice = price;
-    currentPrice = currentPrice - netPrice;
-    document.getElementById(priceId).innerText = currentPrice;
-
-    //subtotal 
-    let subTotal = document.getElementById('subTotal').innerText;
-    subTotal = parseInt(subTotal);
-    subTotal = subTotal - price;
-    document.getElementById('subTotal').innerText = subTotal;
-
-    //Tax and Total amount
-    let tax = (subTotal / 100) * 5;
-    tax = tax.toFixed(2)
-    document.getElementById('tax').innerText = tax;
-    document.getElementById('totalAmount').innerText = totalAmount;
-    const totalAmount = subTotal - tax;
-
-};
-
-
-
-//sub total
-
-let productOne = document.getElementById('currentPrice1').innerText;
-productOne = parseInt(productOne);
-let productTwo = document.getElementById('currentPrice2').innerText;
-productTwo = parseInt(productTwo);
-const subTotal = productOne + productTwo;
-document.getElementById('subTotal').innerText = subTotal;
-
-let tax = (subTotal / 100) * 5;
-tax = tax.toFixed(2);
-document.getElementById('tax').innerText = tax;
-
-const totalAmount = subTotal - tax;
-document.getElementById('totalAmount').innerText = totalAmount;
-
-//checkout
-const checkout = document.getElementById('checkout');
-const welcomePage = document.getElementById('welcomePage');
-welcomePage.style.display = "none";
-checkout.addEventListener('click', function () {
-    const cart = document.getElementById('shoppingCart');
-    cart.style.display = "none";
-    welcomePage.style.display = "block";
+caseDecrease.addEventListener('click', function(){
+    const caseInput = document.getElementById('case-count');
+    const caseCount = parseInt(caseInput.value);
+    const caseNewCount = caseCount - 1;
+    caseInput.value = caseNewCount;
+    const caseTotal = caseNewCount * 59;
+    document.getElementById('case-total').innerText = caseTotal;
 });
