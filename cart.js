25163949b25a65dfa1a04e4725382ed7e17cbcf0
Shakib448 <shakiba448@gmail.com>
2020-07-25 21:26:04 +0600

    first commit

--- cart.js
+++ cart.js
@@ -1,23 +1,46 @@
 // Product function 
 
 function handleProductChange(product, isIncrease){
-    const productInput = document.getElementById(product + '-count');
-    const productCount = parseInt(productInput.value);
-    let caseNewCount = productCount;
+    const productCount = getInputValue(product);
+
+    let productNewCount = productCount;
+
     if(isIncrease == true){
         productNewCount = productCount + 1;
     }
     if(isIncrease == false && productCount > 0){
         productNewCount = productCount -1;
     }
-    productInput.value = productNewCount;
+    document.getElementById(product + '-count').value = productNewCount;
     let productTotal = 0;
 
-    if(product == 'iphone'){
+    if(product == 'phone'){
         productTotal = productNewCount * 1219;
     }
     if(product == 'case'){
         productTotal = productNewCount * 59;
     }
     document.getElementById(product + '-total').innerText = productTotal;
+    calculateTotal();
+};
+
+
+function calculateTotal(){
+    const phoneCount = getInputValue('phone');
+    const caseCount = getInputValue('case');
+
+    const totalPrice = phoneCount * 1219 + caseCount * 59;
+    document.getElementById('total-price').innerText = '$ ' + totalPrice;
+
+    const tax = Math.round((totalPrice * 0.1));
+    document.getElementById('tax-amount').innerText = '$ ' + tax;
+    
+    const grandTotal = totalPrice + tax;
+    document.getElementById('grand-total').innerText = '$ ' + grandTotal;
+};
+
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
 };
